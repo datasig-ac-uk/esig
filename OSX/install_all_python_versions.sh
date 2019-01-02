2f793e2b732c59a6ea5b85bf82a486b8bba823a0
Nick Barlow <nbarlow@turing.ac.uk>
2019-01-02 17:17:36 +0000

    python 3.7

--- OSX/install_all_python_versions.sh
+++ OSX/install_all_python_versions.sh
@@ -4,8 +4,13 @@
 # pyenv can be installed using homebrew:
 # brew install pyenv
 
+# openssl is also needed for python 3.7
+# brew install openssl
+
 eval "$(pyenv init -)"
 
 for p in $( cat python_versions.txt ); do
-	 pyenv install $p
+          CFLAGS="-I$(brew --prefix openssl)/include" \
+	  LDFLAGS="-L$(brew --prefix openssl)/lib" \
+	  pyenv install $p
 done
