73d38a4e738ed56903c0f510004dd3c50b7510b3
Roly Perera <rperera@turing.ac.uk>
2020-02-04 09:35:26 +0000

    Experiment with heredoc but drop for now.

--- build/Linux/build-wheels.sh
+++ build/Linux/build-wheels.sh
@@ -1,8 +1,4 @@
-for arch in x86_64 # i686
+for arch in x86_64 i686
 do 
-   docker run --rm -v ${PWD}:/data esig_builder_linux_${arch} <<-BLOCK
-      source ~/.bashrc
-      cd /data
-      source linux_wheel_maker.sh $arch
-   BLOCK
+   docker run --rm -v ${PWD}:/data esig_builder_linux_${arch} "source ~/.bashrc; cd /data; source linux_wheel_maker.sh $arch"
 done
