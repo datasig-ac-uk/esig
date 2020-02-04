a41bae368366bb6c79c00525be02842cc5cb9389
Roly Perera <rperera@turing.ac.uk>
2020-02-04 19:52:18 +0000

    Better comment.

--- build/Linux/build-wheels.sh
+++ build/Linux/build-wheels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Top-level build script.
 
-for arch in i686 # x86_64 fails with gcc internal compiler error running esig installer
+for arch in i686 # x86_64 installs fail with gcc internal compiler error
 do 
    docker run --rm -v ${PWD}:/data esig_builder_linux_${arch} "source ~/.bashrc; cd /data; source linux_wheel_maker.sh $arch"
 done
