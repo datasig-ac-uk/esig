878ca2d58d6d72480662d2cf9d6649fe1b25db7d
Roly Perera <rperera@turing.ac.uk>
2020-02-04 08:59:59 +0000

    Local .gitignore.

--- build/Linux/build-wheels.sh
+++ build/Linux/build-wheels.sh
@@ -1,4 +1,8 @@
-for arch in  x86_64 # i686
+for arch in x86_64 # i686
 do 
-   docker run --rm -v ${PWD}:/data esig_builder_linux_${arch} "source ~/.bashrc; cd /data; source linux_wheel_maker.sh $arch"
+   docker run --rm -v ${PWD}:/data esig_builder_linux_${arch} <<-BLOCK
+      source ~/.bashrc
+      cd /data
+      source linux_wheel_maker.sh $arch
+   BLOCK
 done
