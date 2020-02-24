8aa53a1f3b7ee91f38651a86cf19aeded9cc7493
Roly Perera <rperera@turing.ac.uk>
2020-02-24 17:13:20 +0000

    Change CI environment to ensure Python 3.5.9 is available.

--- build/sdist.sh
+++ build/sdist.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 # Build source distribution. Run this script from appropriate build subdirectory (Linux or OSX).
-# Previously we got this as a .tar.gz from (https://pypi.org/project/esig/#files). Use a virtualenv
-# at Python 3.5.5 to avoid depending on the local Python version (although requiring 3.5.5 to be
-# available is still not ideal).
+# Previously we got this as a .tar.gz from (https://pypi.org/project/esig/#files). We depend on a
+# local Python installation to build sdist; the CI environment takes care of this.
 
 rm *.tar.gz
 build_dir=$PWD
 pushd ../.. # need to run in same directory as setup.py
-echo "Python version installed:" 
+echo "Python version installed (>= 3.5 required):" 
 python --version
-# pyenv virtualenv 3.5.5 sdist-env-3.5
-# pyenv activate sdist-env-3.5
-#   echo "Python version for building source distribution:" 
-#   python --version
    pip install cython # not present in GitHub macos-10.15 environment
    python setup.py sdist --dist-dir=$build_dir
-#pyenv deactivate
 rm -rf esig.egg-info
 popd
