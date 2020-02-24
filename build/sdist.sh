30013806930447e0af75b4d0b425d448aa840046
Roly Perera <rperera@turing.ac.uk>
2020-02-24 16:58:54 +0000

    Experiment with _not_ using pyenv to run sdist, but instead assuming Python installed; and then using workflow .yml file to specify a Python version.

--- build/sdist.sh
+++ build/sdist.sh
@@ -10,12 +10,12 @@ build_dir=$PWD
 pushd ../.. # need to run in same directory as setup.py
 echo "Python version installed:" 
 python --version
-pyenv virtualenv 3.5.5 sdist-env-3.5
-pyenv activate sdist-env-3.5
-   echo "Python version for building source distribution:" 
-   python --version
+# pyenv virtualenv 3.5.5 sdist-env-3.5
+# pyenv activate sdist-env-3.5
+#   echo "Python version for building source distribution:" 
+#   python --version
    pip install cython # not present in GitHub macos-10.15 environment
    python setup.py sdist --dist-dir=$build_dir
-pyenv deactivate
+#pyenv deactivate
 rm -rf esig.egg-info
 popd
