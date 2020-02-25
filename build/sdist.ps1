b909e5c8f3a421a273daf500ca7755e40dd7fc13
Roly Perera <rperera@turing.ac.uk>
2020-02-25 09:20:08 +0000

    Extend path to pick up 3.5 installation.

--- build/sdist.ps1
+++ build/sdist.ps1
@@ -3,6 +3,8 @@
 rm *.tar.gz
 build_dir=$PWD
 pushd ../.. # need to run in same directory as setup.py
+# assumes container has Python 3.5 installed
+$Env:Path += ";C:\Users\ContainerAdministrator\AppData\Local\Programs\Python\Python35;C:\Users\ContainerAdministrator\AppData\Local\Programs\Python\Python35\Scripts"
 echo "Python version installed (>= 3.5 required):" 
 python.exe --version
    pip install cython # not present in GitHub macos-10.15 environment
