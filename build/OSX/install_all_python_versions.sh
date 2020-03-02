d74306d8d7f1d693682e28a2cffb7fa46569a28e
Roly Perera <rperera@turing.ac.uk>
2020-03-02 17:40:50 +0000

    Experiment with hosted build without openssl installed.

--- build/OSX/install_all_python_versions.sh
+++ build/OSX/install_all_python_versions.sh
@@ -5,7 +5,10 @@ eval "$(pyenv init -)"
 
 for p in $( cat python_versions.txt ); do
    # see https://github.com/pyenv/pyenv/wiki/common-build-problems
-   CFLAGS="-I$(brew --prefix openssl)/include" \
-   LDFLAGS="-L$(brew --prefix openssl)/lib" \
+   # CFLAGS="-I$(brew --prefix openssl)/include" \
+   # LDFLAGS="-L$(brew --prefix openssl)/lib" \
+   # CFLAGS="-I./libressl-2.2.7/include" \ 
+   # CPPFLAGS="-I./libressl-2.2.7/include" \
+   # LDFLAGS="-L./libressl-2.2.7/lib" \
    pyenv install --skip-existing $p
 done
