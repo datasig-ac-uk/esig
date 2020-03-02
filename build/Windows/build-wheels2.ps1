867abaa6b1fcbff409c347aa75464a1813a63e62
Roly Perera <rperera@turing.ac.uk>
2020-03-02 11:02:03 +0000

    Runs from root of repo...

--- build/Windows/build-wheels2.ps1
+++ build/Windows/build-wheels2.ps1
@@ -1,6 +1,7 @@
-# Work-in-progress
+# Work-in-progress. Run from root of repo.
 git config --global url."https://github.com/".insteadOf "git@github.com:"
 $auth_header="$(git config --local --get http.https://github.com/.extraheader)"
 git submodule sync --recursive
 git -c "http.extraheader=$auth_header" -c protocol.version=2 submodule update --init --force --recursive --depth=1
+cd build\Windows
 .\windows_wheel_maker2.bat esig-0.6.31.tar.gz python35_32
