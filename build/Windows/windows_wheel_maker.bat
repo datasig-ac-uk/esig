5587178c4a308f61c6bec720bd7b359cbf5bc3a5
Roly Perera <rperera@turing.ac.uk>
2020-03-02 09:42:46 +0000

    Comment out correct file...

--- build/Windows/windows_wheel_maker.bat
+++ build/Windows/windows_wheel_maker.bat
@@ -5,14 +5,14 @@ del *.whl
 if not exist "C:\data\output" mkdir C:\data\output
 python.exe -m pip install virtualenv
 REM build the wheel
-REM python.exe -m pip wheel --no-binary -b latest %1
+python.exe -m pip wheel --no-binary -b latest %1
 REM create a virtualenv for testing.
-REM virtualenv.exe %2
+virtualenv.exe %2
 REM using the virtualenv python, install the newly created esig wheel
-REM FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b esig*.whl`) DO %2\Scripts\python.exe -m pip install %%F
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b esig*.whl`) DO %2\Scripts\python.exe -m pip install %%F
 REM run the tests
-REM %2\Scripts\python.exe -c "import esig.tests as tests; tests.run_tests(terminate=True)"
+%2\Scripts\python.exe -c "import esig.tests as tests; tests.run_tests(terminate=True)"
 REM if tests pass, move the wheel to 'output'
-REM if %ERRORLEVEL% EQU 0 move esig*.whl C:\data\output\
+if %ERRORLEVEL% EQU 0 move esig*.whl C:\data\output\
 REM remove extraneous wheels
-REM del *.whl
+del *.whl
