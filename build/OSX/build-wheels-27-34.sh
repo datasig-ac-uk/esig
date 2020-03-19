2c449e847329358449e91b279a6d107222281b52
Roly Perera <rperera@turing.ac.uk>
2020-03-19 15:15:18 +0000

    Consolidate.

--- build/OSX/build-wheels-27-34.sh
+++ build/OSX/build-wheels-27-34.sh
@@ -45,27 +45,30 @@ sudo /opt/local/bin/port -N install boost
 brew install boost
 brew install openssl # required now?
 
-# TODO: factor out commonality.
+# TODO: relax version checks below to major.minor.
+
 # Python 2.7 (preinstalled)
-# pyexe=/usr/local/bin/python
-# py=$($pyexe --version 2>&1)
-# p=${py##* }
-# if [ $p == "2.7.17" ]
-# then
-#    . mac_wheel_builder-27-34.sh p
-# else
-#    echo "Expecting Python 2.7.17, got $p"
-#    exit 1
-# fi
+pyexe=/usr/local/bin/python
+py=$($pyexe --version 2>&1)
+p=${py##* }
+expect="2.7.17"
+if [ $p == $expect ]
+then
+   . mac_wheel_builder-27-34.sh p ""
+else
+   echo "Expecting Python $expect, got $p"
+   exit 1
+fi
 
 # Python 3.4 (MacPorts)
 pyexe=/opt/local/bin/python3.4 # MacPorts seems to consistently install here
 py=$($pyexe --version 2>&1)
 p=${py##* }
-if [ $p == "3.4.10" ]
+expect="3.4.10"
+if [ $p == $expect ]
 then
    . mac_wheel_builder-27-34.sh p "sudo"
 else
-   echo "Expecting Python 3.4.10, got $p"
+   echo "Expecting Python $expect, got $p"
    exit 1
 fi
