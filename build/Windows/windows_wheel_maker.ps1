43047c972f834b1d8d618748eacd13adb14fb884
Roly Perera <rperera@turing.ac.uk>
2020-03-29 19:36:22 +0100

    Boost root now looks like it has header files in..

--- build/Windows/windows_wheel_maker.ps1
+++ build/Windows/windows_wheel_maker.ps1
@@ -34,10 +34,6 @@ $BOOST_ROOT='.\boost\boost_1_68_0'
 mkdir $BOOST_ROOT\x64
 mkdir $BOOST_ROOT\x64\lib
 
-ls $BOOST_ROOT
-# ABORT ABORT ABORT
-exit 0
-
 Move-Item -Path C:\local\boost_1_68_0\lib64-msvc-14.0\*.lib -Destination $BOOST_ROOT\x64\lib
 
 curl -L -O https://www.python.org/ftp/python/3.5.4/python-3.5.4-amd64.exe
