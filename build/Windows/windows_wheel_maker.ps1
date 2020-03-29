311db88e2ba26ca9e98aff20bf2bdbdae488d736
Roly Perera <rperera@turing.ac.uk>
2020-03-29 09:44:45 +0100

    Now let's see if VS 14.0 folder exists anyway - may be part of windows-2016 image?

--- build/Windows/windows_wheel_maker.ps1
+++ build/Windows/windows_wheel_maker.ps1
@@ -3,12 +3,12 @@
 # del *.whl
 # if not exist "C:\data\output" mkdir C:\data\output
 curl -O https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe
-.\BuildTools_Full.exe /silent /full /passive
 if ( Test-Path -Path 'C:\Program Files (x86)\Microsoft Visual Studio 14.0' -PathType Container ) {
-   echo Visual Studio 14.0 folder exists.
+   echo 'Visual Studio 14.0 folder exists.'
 } else {
    exit 1
 }
+.\BuildTools_Full.exe /silent /full /passive
 python.exe -m pip install virtualenv
 # build the wheel
 pushd ..
