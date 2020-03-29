7163060ba0e24da35b6e68f137c3dfddaa71d2ac
Roly Perera <rperera@turing.ac.uk>
2020-03-29 17:47:05 +0100

    In esig/build/Windows. Look at content of esig/build and esig.

--- build/Windows/windows_wheel_maker.ps1
+++ build/Windows/windows_wheel_maker.ps1
@@ -3,6 +3,7 @@ Set-PSDebug -Trace 1
 pwd
 ls ../
 ls ../../
+exit 0
 
 # run from C:\data\ directory inside the esig_builder_windows docker container.
 # arguments: <python_version_string e.g. python37_64>
