070b62e1a47e90e26f9b5849fe86382aff0a8811
Roly Perera <rperera@turing.ac.uk>
2020-03-29 09:32:07 +0100

    Switch to Powershell.

--- build/Windows/windows_wheel_maker2.bat
+++ build/Windows/windows_wheel_maker2.bat
@@ -6,6 +6,7 @@ if not exist "C:\data\output" mkdir C:\data\output
 curl -O https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe
 .\BuildTools_Full.exe /silent /full /passive
 echo Installed.
+REM probably a better way to have dir emit results to console
 dir 'C:\Program Files (x86)' > blah.txt
 type blah.txt
 rm blah.txt
