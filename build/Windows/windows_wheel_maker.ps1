30ed04b571e8d7fa14891eb8ab009e13c3cb9ef3
Roly Perera <rperera@turing.ac.uk>
2020-03-30 18:14:36 +0100

    First thing didn't work.

--- build/Windows/windows_wheel_maker.ps1
+++ build/Windows/windows_wheel_maker.ps1
@@ -55,12 +55,12 @@ popd
 if ($LASTEXITCODE -ne 0) { throw "pip wheel failed." }
 
 create a virtualenv for testing.
-$venv='.\python35_64'
+$venv='\python35_64'
 $pyexe -m virtualenv $venv
 # using the virtualenv python, install the newly created esig wheel
 $wheel=(ls output\*.whl | Select-Object -First 1).Name
 echo $wheel
-$venv\Scripts\python.exe -m pip install $wheel
+.\$venv\Scripts\python.exe -m pip install $wheel
 # run the tests
 # $target\Scripts\python.exe -c "import esig.tests as tests; tests.run_tests(terminate=True)"
 # if ($LASTEXITCODE -ne 0) { throw "Tests failed." }
