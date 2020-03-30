f547f0806f4b00c31b74750a9dc27116ba88264a
Roly Perera <rperera@turing.ac.uk>
2020-03-30 17:54:43 +0100

    Enable a bit more.

--- build/Windows/windows_wheel_maker.ps1
+++ build/Windows/windows_wheel_maker.ps1
@@ -60,7 +60,7 @@ virtualenv.exe $target
 # using the virtualenv python, install the newly created esig wheel
 $wheel=(ls output\*.whl | Select-Object -First 1).Name
 echo $wheel
-$target\Scripts\python.exe -m pip install $wheel
+# $target\Scripts\python.exe -m pip install $wheel
 # run the tests
 # $target\Scripts\python.exe -c "import esig.tests as tests; tests.run_tests(terminate=True)"
 # if ($LASTEXITCODE -ne 0) { throw "Tests failed." }
