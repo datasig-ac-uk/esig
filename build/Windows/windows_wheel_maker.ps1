20e95e7b1bbf02ecb6cf9c47499fa9c5ec33a2a4
Roly Perera <rperera@turing.ac.uk>
2020-03-30 18:48:28 +0100

    Wtf?

--- build/Windows/windows_wheel_maker.ps1
+++ build/Windows/windows_wheel_maker.ps1
@@ -20,9 +20,11 @@ Set-PSDebug -Trace 1
 $ENV:BOOST_ROOT='C:\boost\boost_1_68_0\'
 
 # Boost binaries.
+# curl -L -o boost_1_68_0-msvc-14.0-64.exe https://sourceforge.net/projects/boost/files/boost-binaries/1.68.0/boost_1_68_0-msvc-14.0-64.exe/download
 curl -L -o boost_1_68_0-msvc-14.1-64.exe https://sourceforge.net/projects/boost/files/boost-binaries/1.68.0/boost_1_68_0-msvc-14.1-64.exe/download
 
 # self-extracting installer - will unpack to C:\local\boost\boost_1_68_0\lib[64,32]-msvc-[version]
+# without /VERYSILENT installer will attempt to open dialog box and silently fail
 Start-Process -Wait -PassThru -FilePath .\boost_1_68_0-msvc-14.1-64.exe -ArgumentList '/VERYSILENT /SP-'
 
 mkdir $ENV:BOOST_ROOT\x64
@@ -40,9 +42,15 @@ $ENV:PATH="C:\Users\runneradmin\AppData\Local\Programs\Python\Python35;C:\Users\
 # TODO: check 3.5[.4]
 python --version
 
-foreach ($package in @("numpy","wheel","delocate","setuptools","virtualenv")) {
-   python -m pip install $package
-}
+# foreach ($package in @("numpy","wheel","delocate","setuptools","virtualenv")) {
+#   python -m pip install $package
+# }
+
+python -m pip install numpy
+python -m pip install wheel
+python -m pip install delocate
+python -m pip install --upgrade setuptools
+python -m pip install virtualenv
 
 # build the wheel
 pushd ..
@@ -50,7 +58,7 @@ pushd ..
 popd
 if ($LASTEXITCODE -ne 0) { throw "pip wheel failed." }
 
-# create a virtualenv for testing.
+create a virtualenv for testing.
 $venv='python35_64'
 python -m virtualenv $venv
 # using the virtualenv python, install the newly created esig wheel
