00f528c9102f2ff4513ee33f9f8568c3fae3a193
Roly Perera <rperera@turing.ac.uk>
2020-05-05 15:08:36 +0100

    Reinstate Python 3.4 on Linux.

--- build/OSX/build-wheels.sh
+++ build/OSX/build-wheels.sh
@@ -3,4 +3,7 @@
 # Each of these runs as a parallel job in the GitHub runner.
 . build-wheels-27.sh
 . build-wheels-34.sh
-. build-wheels-rest.sh
+. build-wheels-rest.sh 3.5.5
+. build-wheels-rest.sh 3.6.5
+. build-wheels-rest.sh 3.7.0
+. build-wheels-rest.sh 3.8.1
