9b5424157d8c3f6ad2849466f64cfd852eb57b89
Roly Perera <rperera@turing.ac.uk>
2020-05-11 16:03:25 +0100

    New top-level local Linux build script. Consolidate 'build-wheel' naming convention.

--- build/OSX/build-wheels.sh
+++ build/OSX/build-wheels.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 # Each of these runs as a parallel job in the GitHub runner.
-. build-wheels-27.sh
-. build-wheels-34.sh
-. build-wheels-rest.sh 3.5.5
-. build-wheels-rest.sh 3.6.5
-. build-wheels-rest.sh 3.7.0
-. build-wheels-rest.sh 3.8.1
+. build-wheel-27.sh
+. build-wheel-34.sh
+. build-wheel-other.sh 3.5.5
+. build-wheel-other.sh 3.6.5
+. build-wheel-other.sh 3.7.0
+. build-wheel-other.sh 3.8.1
