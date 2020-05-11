8641ea4b5eca293ed779faadee66f633ec8b3a5e
Roly Perera <rperera@turing.ac.uk>
2020-05-11 12:13:46 +0100

    Trace output. Try giving Twine a folder rather than folder/*.

--- build/publish.sh
+++ build/publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 # Publish to PyPI. Publish step will fail unless version number is bumped (currently a manual step).
+set -u -o xtrace
 
 ls -la dist/
 python -m pip install --upgrade twine==3.1.1
-python -m twine upload -u __token__ -p {{ secrets.testpypi_password }} -r testpypi dist/*
+python -m twine upload -u __token__ -p {{ secrets.testpypi_password }} -r testpypi dist/
 echo Uploaded to PyPI.
