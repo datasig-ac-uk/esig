633f4effcd2dc3b1004e8fd33d3e44d282d6ddb5
rolyp <rperera@turing.ac.uk>
2020-06-16 13:48:46 +0100

    Try passing user name, password to HTTPS URL, providing password as an argument (which I'll store at a GitHub secret).

--- build/git-preamble.sh
+++ build/git-preamble.sh
@@ -1,5 +1,10 @@
+#!/bin/bash -e
+# $1: my GitHub password
+set -u -o xtrace
+
 git config --global url."https://github.com/".insteadOf "git@github.com:"
 auth_header="$(git config --local --get http.https://github.com/.extraheader)"
 git submodule sync --recursive
 git -c "http.extraheader=$auth_header" -c protocol.version=2 submodule update --init --force --recursive --depth=1
-git clone https://github.com/terrylyons/recombine.git build/recombine
+
+git clone https://rolyp:$1@github.com/terrylyons/recombine.git build/recombine
