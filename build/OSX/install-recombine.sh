1d5371688dc01c98f978585d3660d68db2ebf8b9
rolyp <rperera@turing.ac.uk>
2020-06-16 13:01:09 +0100

    Experiment with http.extraheader as per submodule script.

--- build/OSX/install-recombine.sh
+++ build/OSX/install-recombine.sh
@@ -2,11 +2,9 @@
 # Build recombine from sources and install.
 set -u -o xtrace
 
-rm -rf recombine
-# git config user.email "email"
-git config --global url."git@github.com:".insteadOf https://github.com/
-git config user.name "rolyp"
-git clone git@github.com:terrylyons/recombine.git
+# Only do this step if running locally:
+# rm -rf recombine
+# git clone git@github.com:terrylyons/recombine.git
 
 pushd recombine
 ./doall-macOS.sh
