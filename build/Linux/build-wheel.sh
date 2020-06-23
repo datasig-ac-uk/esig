ab162def4b22b8529ba54f2c398845dbdbb68169
rolyp <rperera@turing.ac.uk>
2020-06-23 19:26:09 +0100

    Add recombine build step to Linux build.

--- build/Linux/build-wheel.sh
+++ build/Linux/build-wheel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
-set -u -o xtrace
 # Top-level script for building esig for Linux on MacOS or Linux.
 # $1: Python version
 # $2: one of {i686, x86_64}
 # Python wheels will be created for specified architecture, for each Python version in python_versions.txt.
+set -u -o xtrace
+
 if [[ $2 == "i686" ]] || [[ $2 == "x86_64" ]]
 then
    arch=$2
@@ -11,6 +12,10 @@ else
    echo "Invalid architecture"
    exit 1
 fi
+
+pushd ../recombine
+./doall-linux.sh
+popd
 
 docker build -t esig_builder_linux_${arch} -f Dockerfile_${arch}.dockerfile .
 
