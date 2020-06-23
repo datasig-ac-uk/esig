3e7eafa511f2c8f3d5aeb4143a00772ab85c4059
rolyp <rperera@turing.ac.uk>
2020-06-23 10:59:18 +0100

    Concatentation instead of interpolation.

--- setup.py
+++ setup.py
@@ -12,8 +12,11 @@ configuration.package_abs_root = os.path.dirname(os.path.realpath(__file__))
 home = os.environ["HOME"]
 
 if configuration.platform == helpers.PLATFORMS.MACOS:
-   # https://stackoverflow.com/questions/2584595/building-a-python-module-and-linking-it-against-a-macosx-framework
-   os.environ['LDFLAGS'] = f'-F {home}/lyonstech/ -framework recombine -Wl,-rpath,{home}/lyonstech/'
+    # https://stackoverflow.com/questions/2584595/building-a-python-module-and-linking-it-against-a-macosx-framework
+    os.environ['LDFLAGS'] =
+        '-F ' + home + '/lyonstech/ ' +
+        '-framework recombine ' +
+        '-Wl,-rpath,' + home + '/lyonstech/'
 
 esig_extension = Extension(
     'esig.tosig',
