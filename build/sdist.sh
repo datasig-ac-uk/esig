9cf46a39f571e1906bd1dcdc5871de24aea021f2
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2020-12-11 12:35:11 +0000

    Update sdist.sh
    Fixed accidentally removed build command

--- build/sdist.sh
+++ build/sdist.sh
@@ -7,6 +7,8 @@ pushd .. # need to run in same directory as setup.py
 echo "Python version installed (>= 3.5 required):"
 python --version
 # Cython not present in GitHub macos-10.15 environment
+python setup.py sdist --dist-dir=build/sdist
+
 
 rm -rf esig.egg-info
 popd
