5ecb1dbe1a33a65ab39314309175246414f1dfb7
rolyp <rperera@turing.ac.uk>
2021-02-17 18:57:51 +0000

    Simplify run_tests. Print out number of test cases; also test result.

--- esig/tests/__init__.py
+++ esig/tests/__init__.py
@@ -4,24 +4,6 @@ import unittest
 from . import test_package
 
 
-def get_suite():
-    """
-    Constructs and returns a Python unittest suite object.
-    This can be used to run the unit tests for ESig.
-
-    Args:
-        None
-    Returns:
-        suite: A Python unittest suite, referring to all tests specified within the tests package.
-    """
-    package_root = os.path.dirname(os.path.abspath(__file__))
-    suite = unittest.TestLoader().discover(package_root)
-    #suite.addTest(unittest.makeSuite(test_package.TestESIG))
-    #suite.addTest(unittest.makeSuite(test_package.TestRecombine))
-
-    return suite
-
-
 def run_tests(terminate=False): # argument ignored
     """
     Acquires the ESig test suite, and runs the tests.
@@ -29,13 +11,14 @@ def run_tests(terminate=False): # argument ignored
     Returns:
         terminates with exit code 1 if all tests pass successfully; 0 otherwise.
     """
-    suite = get_suite()
+    package_root = os.path.dirname(os.path.abspath(__file__))
+    suite = unittest.TestLoader().discover(package_root)
+    print(suite.countTestCases() + " test cases found.")
     runner = unittest.TextTestRunner()
-    status = runner.run(suite).wasSuccessful()
+    result = runner.run(suite)
+    print(result)
 
-    if not status:
+    if not result.wasSuccessful():
         sys.exit(1)
     else:
         sys.exit(0)
-
-    #unittest.main(module="esig.tests")
