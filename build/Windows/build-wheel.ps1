1c66305d0d09f71f4d9d12c485ac0c82fc776ba3
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2021-04-07 13:35:11 +0100

    Update build-wheel.ps1

--- build/Windows/build-wheel.ps1
+++ build/Windows/build-wheel.ps1
@@ -81,14 +81,13 @@ $wheel=(ls wheeldir\*.whl | Select-Object -First 1).Name
 
 echo $wheel
 
-ls .\venv\Scripts
 .\venv\Scripts\python.exe -m pip install wheeldir\$wheel
 
 # run tests
 # TODO: Python 3.8+ doesn't use PATH to find dependent DLLs
 
 echo (Invoke-Expression ".\venv\Scripts\python.exe --version")
-.\venv\Scripts\python.exe -m unittest discover -v -s ..\..\esig\tests
+.\venv\Scripts\python.exe -vvv -X faulthandler -m unittest discover -v -s ..\..\esig\tests
 
 if ($LASTEXITCODE -ne 0) {
    throw "Tests failed - will not copy wheel to output"
