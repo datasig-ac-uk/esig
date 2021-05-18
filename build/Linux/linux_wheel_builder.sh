6c6cfb172c431329671bfeec8b3de4a5034738c4
Sam Morley <41870650+inakleinbottle@users.noreply.github.com>
2021-05-18 14:09:45 +0100

    Set more recent version of numpy

--- build/Linux/linux_wheel_builder.sh
+++ build/Linux/linux_wheel_builder.sh
@@ -34,7 +34,7 @@ pyexe=/opt/python/$py/bin/python
 ls -la /opt/python # see what Python versions there are
 $pyexe -m pip install -U pip virtualenv
 $pyexe -m pip install -U 'wheel>=0.34.2' # need at least this version for auditwheel 3.1.1 (Python 3.7)
-$pyexe -m pip install --no-cache-dir --only-binary :all: 'numpy>=1.7.0'
+$pyexe -m pip install --no-cache-dir --only-binary :all: 'numpy>=1.17.0'
 
 # recombine build step will have left library here
 recombine_install_dir=${HOME}/lyonstech
