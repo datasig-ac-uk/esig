15c6e9e4b4784247bfe975047af6a7762428c1a1
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2021-05-20 18:26:36 +0100

    Update build-wheel.sh

--- build/Linux/build-wheel.sh
+++ build/Linux/build-wheel.sh
@@ -13,14 +13,6 @@ else
    exit 1
 fi
 
-# Increase the size of the swap file to help with the build
-SWAP_PATH=/extra_swap
-sudo fallocate -l 4G ${SWAP_PATH}
-sudo chmod 600 ${SWAP_PATH}
-sudo mkswap ${SWAP_PATH}
-sudo swapon ${SWAP_PATH}
-echo "${SWAP_PATH}   none   swap   sw   0   0" | sudo tee /etc/fstab -a 
-
 
 # Use recombine as base image for esig
 pushd ../recombine
