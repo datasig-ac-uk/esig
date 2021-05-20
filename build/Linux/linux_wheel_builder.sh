1160d310bc06e977d236bae226a114b06b298af7
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2021-05-20 18:26:25 +0100

    Update linux_wheel_builder.sh

--- build/Linux/linux_wheel_builder.sh
+++ build/Linux/linux_wheel_builder.sh
@@ -24,15 +24,6 @@ popd
 TMPDIR=tmp         # working folder for pip wheel
 OUTPUTDIR=output   # location of tested wheels
 
-# Increase the size of the swap file to help with the build
-SWAP_PATH=/extra_swap
-sudo fallocate -l 4G ${SWAP_PATH}
-sudo chmod 600 ${SWAP_PATH}
-sudo mkswap ${SWAP_PATH}
-sudo swapon ${SWAP_PATH}
-echo "${SWAP_PATH}   none   swap   sw   0   0" | sudo tee /etc/fstab -a 
-
-
 rm -rf $TMPDIR
 rm -rf wheelhouse
 rm -rf $OUTPUTDIR
