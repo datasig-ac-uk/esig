a05cef59cf3433c0d047790ae846db6a86f30801
Sam Morley <sam@inakleinbottle.com>
2021-08-04 17:02:17 +0100

    Improvements for performance

--- src/ToSig.cpp
+++ src/ToSig.cpp
@@ -44,98 +44,72 @@ namespace {
 	}
   */
 
-  /**
-   * GetLogSignature
-   * @param stream pointer to stream as PyArrayObject, assumed to have two dimensions, the row is assumed to be of length WIDTH
-   * @return the log signature of the stream as LIE
-   */
-	template <class LIE, class CBH, size_t WIDTH>
-	LIE GetLogSignature(PyArrayObject *stream)
-	{
-	  npy_intp numRows = PyArray_DIM(stream, 0);
-		std::vector<LIE> increments;
-		if (numRows > 0) {
-		  npy_intp rowId = 0;
-		  LIE previous = row_to_lie<LIE, WIDTH>(stream,rowId++);
+    /**
+     * GetLogSignature
+     * @param stream pointer to stream as PyArrayObject, assumed to have two dimensions, the row is assumed to be of length WIDTH
+     * @return the log signature of the stream as LIE
+     */
+    template<class LIE, class CBH, size_t WIDTH>
+    LIE GetLogSignature(PyArrayObject *stream) {
+        using lie_increment = lie_increment_t<WIDTH>;
+        npy_intp numRows = PyArray_DIM(stream, 0);
+        if (numRows <= 1) {
+            return LIE();
+        }
 
-		  for (; rowId < numRows; ++rowId) {
-			  LIE next(row_to_lie<LIE, WIDTH>(stream,rowId));
-			  increments.push_back(next - previous);
-			  previous = next;
-		  }
-		}
+        std::vector<lie_increment> increments;
+
+        npy_intp rowId = 0;
+        lie_increment previous = row_to_lie<WIDTH>(stream, rowId++);
+
+        for (; rowId < numRows; ++rowId) {
+            lie_increment next(row_to_lie<WIDTH>(stream, rowId));
+            increments.push_back(next - previous);
+            previous = next;
+        }
+
+        CBH cbh;
+        return cbh.full(increments.begin(), increments.end());
+    }
+
+
+    /**
+     * KeyToIndexRec - recursive helper function used in KeyToIndex
+     */
+    template<class TENSOR, size_t WIDTH>
+    inline std::pair<size_t, typename TENSOR::BASIS::KEY> KeyToIndexRec(size_t i, typename TENSOR::BASIS::KEY k) {
+        return k.size() ? KeyToIndexRec<TENSOR, WIDTH>(i * WIDTH + k.FirstLetter(), k.rparent()) : std::make_pair(i, k);
+    }
+
+    /**
+     * KeyToIndex - computes the position of a key in the vectorised tensor
+     * @param k tensor key
+     * @return position as index
+     */
+    template<class TENSOR, size_t WIDTH>
+    inline size_t KeyToIndex(typename TENSOR::BASIS::KEY k) {
+        return KeyToIndexRec<TENSOR, WIDTH>(0, k).first;
+        //return (k.size()) ? k.FirstLetter() + WIDTH * KeyToIndex<TENSOR, WIDTH>(k.rparent()) : 0; // incorrect version, commented out
+    }
+
+    /*
+      template <class TENSOR, size_t WIDTH>
+      inline size_t KeyToIndex(typename TENSOR::BASIS::KEY k)
+      {
+          return (k.size()) ? k.FirstLetter() + WIDTH * KeyToIndex<TENSOR,
+              WIDTH>(k.rparent()) : 0;
+      }
+    */
+
+    template<class VECTOR, class TENSOR, size_t WIDTH>
+    struct fn0001 {
+        VECTOR &_ans;
+
+        fn0001(VECTOR &ans) : _ans(ans) {
+        }
+
 #ifndef LIBALGEBRA_VECTORS_H
-		std::vector<LIE*> pincrements;
-#else
-		std::vector<const LIE*> pincrements;
-#endif
-		pincrements.reserve(increments.size());
-		for (typename std::vector<LIE>::iterator it = increments.begin();
-			it != increments.end(); ++it)
-		  pincrements.push_back(&(*it));
-		CBH cbh;
-		return (pincrements.size() != 0) ? cbh.full(pincrements) : LIE();
-	}
-  /*
-	template <class LIE, class STATE, class CBH, size_t WIDTH>
-	LIE GetLogSignature(const STATE* begin, const STATE* end)
-	{
-		std::vector<LIE> increments;
-		if (begin != end) {
-			const STATE* it = begin;
-			LIE previous = vector_to_lie<LIE, STATE, WIDTH>(*(it++));
-
-			for (; it != end; ++it) {
-				LIE next(vector_to_lie<LIE, STATE, WIDTH> (*it));
-				increments.push_back(next - previous);
-				previous = next;
-			}
-		}
-		std::vector<LIE*> pincrements;
-		for (typename std::vector<LIE>::iterator it = increments.begin();
-			it != increments.end(); ++it)
-			pincrements.push_back(&(*it));
-		CBH cbh;
-		return (pincrements.size() != 0) ? cbh.full(pincrements) : LIE();
-	}
-  */
-
-  /**
-   * KeyToIndexRec - recursive helper function used in KeyToIndex
-   */
-	template <class TENSOR, size_t WIDTH>
-	inline std::pair<size_t, typename TENSOR::BASIS::KEY> KeyToIndexRec(size_t i,typename TENSOR::BASIS::KEY k){	
-		return k.size() ? KeyToIndexRec<TENSOR,WIDTH>(i*WIDTH+k.FirstLetter(),k.rparent()) : std::make_pair(i,k) ;
-	}
-
-  /**
-   * KeyToIndex - computes the position of a key in the vectorised tensor
-   * @param k tensor key
-   * @return position as index
-   */
-	template <class TENSOR, size_t WIDTH>
-	inline size_t KeyToIndex(typename TENSOR::BASIS::KEY k)
-	{		
-		return KeyToIndexRec<TENSOR,WIDTH>(0,k).first;
-		//return (k.size()) ? k.FirstLetter() + WIDTH * KeyToIndex<TENSOR, WIDTH>(k.rparent()) : 0; // incorrect version, commented out
-	}
-  /*
-	template <class TENSOR, size_t WIDTH>
-	inline size_t KeyToIndex(typename TENSOR::BASIS::KEY k)
-	{
-		return (k.size()) ? k.FirstLetter() + WIDTH * KeyToIndex<TENSOR,
-			WIDTH>(k.rparent()) : 0;
-	}
-  */
-
-	template <class VECTOR, class TENSOR, size_t WIDTH>
-	struct fn0001 {
-		VECTOR& _ans;
-		fn0001(VECTOR& ans):_ans(ans)
-		{
-		}
-#ifndef LIBALGEBRA_VECTORS_H
-		template <class T>
+        template <class T>
         void operator()(T& element)
         {
             _ans[KeyToIndex<TENSOR, WIDTH>(element.first)] = element.second;
@@ -146,13 +120,14 @@ namespace {
         {
 		    _ans[KeyToIndex<TENSOR, WIDTH>(element.key())] = element.value();
         }
+
 #endif
 
-	};
+    };
 
-	//[&ans] (const decltype(*(arg.begin()))& element){
-	//	ans[KeyToIndex<TENSOR, WIDTH>(element.first)] = element.second;
-	//}
+    //[&ans] (const decltype(*(arg.begin()))& element){
+    //	ans[KeyToIndex<TENSOR, WIDTH>(element.first)] = element.second;
+    //}
 
 
 	template <class S, class TENSOR, size_t WIDTH, size_t DEPTH>
@@ -172,27 +147,28 @@ namespace {
 		  *((double *)PyArray_GETPTR1(snk,i)) = ans[i];
 	}
 
-	template <class VECTOR>
-	struct fn0002 {
-		VECTOR& _ans;
-		fn0002(VECTOR& ans):_ans(ans)
-		{
-		}
+    template<class VECTOR>
+    struct fn0002 {
+        VECTOR &_ans;
+
+        fn0002(VECTOR &ans) : _ans(ans) {
+        }
 
 #ifndef LIBALGEBRA_VECTORS_H
-		template <class T>
+        template <class T>
         void operator()(T& element)
         {
             _ans[element.first - 1] = element.second;
         }
 #else
-        template <class T>
-        void operator()(T& element)
-        {
+
+        template<class T>
+        void operator()(T &element) {
             _ans[element.key() - 1] = element.value();
         }
+
 #endif
-	};
+    };
 
 	template <class S, class LIE, size_t WIDTH, size_t DEPTH>
 	void unpack_lie_to_SNK(const LIE& arg, PyArrayObject *snk)
@@ -218,14 +194,14 @@ namespace {
 		//typedef alg::free_tensor<S, Q, WIDTH, DEPTH> TENSOR;
 		typedef alg::lie<S, Q, WIDTH, DEPTH> LIE;
 
-		LIE::basis.growup(DEPTH);
+        LIE::basis.growup(DEPTH);
 
-		std::string ans;
-		for (typename LIE::BASIS::KEY k = LIE::basis.begin(); k != LIE::basis.end();
-			k = LIE::basis.nextkey(k))
-			ans += std::string(" ") + LIE::basis.key2string(k);
-		return ans;
-	}
+        std::string ans;
+        for (typename LIE::BASIS::KEY k = LIE::basis.begin(); k != LIE::basis.end();
+             k = LIE::basis.nextkey(k))
+            ans += std::string(" ") + LIE::basis.key2string(k);
+        return ans;
+    }
 
 	template <size_t WIDTH, size_t DEPTH>
 	std::string tensorbasis2stringT()
@@ -235,13 +211,13 @@ namespace {
 		typedef alg::free_tensor<S, Q, WIDTH, DEPTH> TENSOR;
 		//typedef alg::lie<S, Q, WIDTH, DEPTH> LIE;
 
-		std::string ans;
-		for (typename TENSOR::BASIS::KEY k = TENSOR::basis.begin();
-			k < TENSOR::basis.end(); k = TENSOR::basis.nextkey(k))
-			ans += std::string(" (") + TENSOR::basis.key2string(k) +
-				std::string(")");
-		return ans;
-	}
+        std::string ans;
+        for (typename TENSOR::BASIS::KEY k = TENSOR::basis.begin();
+             k < TENSOR::basis.end(); k = TENSOR::basis.nextkey(k))
+            ans += std::string(" (") + TENSOR::basis.key2string(k) +
+                   std::string(")");
+        return ans;
+    }
 
   /**
    * GetSigT - computes the signature of a stream into snk
@@ -262,26 +238,26 @@ namespace {
 		return true;
 	}
 
-  /*
-	template <size_t WIDTH, size_t DEPTH>
-	bool GetSigT(const double* src, double* snk, size_t recs)
-	{
-		typedef const double array[WIDTH];
-		const array* source = reinterpret_cast<const array*>(src);
-		typedef double S;
-		typedef double Q;
-		typedef alg::free_tensor<S, Q, WIDTH, DEPTH> TENSOR;
-		typedef alg::lie<S, Q, WIDTH, DEPTH> LIE;
-		typedef alg::maps<S, Q, WIDTH, DEPTH> MAPS;
-		typedef alg::cbh<S, Q, WIDTH, DEPTH> CBH;
-		LIE logans = GetLogSignature<LIE, array, CBH, WIDTH>(&source[0],
-			&source[0 + recs]);
-		MAPS maps;
-		TENSOR signature = exp(maps.l2t(logans));
-		unpack_tensor_to_SNK<S, TENSOR, WIDTH, DEPTH>(signature, snk);
-		return true;
-	}
-  */
+    /*
+      template <size_t WIDTH, size_t DEPTH>
+      bool GetSigT(const double* src, double* snk, size_t recs)
+      {
+          typedef const double array[WIDTH];
+          const array* source = reinterpret_cast<const array*>(src);
+          typedef double S;
+          typedef double Q;
+          typedef alg::free_tensor<S, Q, WIDTH, DEPTH> TENSOR;
+          typedef alg::lie<S, Q, WIDTH, DEPTH> LIE;
+          typedef alg::maps<S, Q, WIDTH, DEPTH> MAPS;
+          typedef alg::cbh<S, Q, WIDTH, DEPTH> CBH;
+          LIE logans = GetLogSignature<LIE, array, CBH, WIDTH>(&source[0],
+              &source[0 + recs]);
+          MAPS maps;
+          TENSOR signature = exp(maps.l2t(logans));
+          unpack_tensor_to_SNK<S, TENSOR, WIDTH, DEPTH>(signature, snk);
+          return true;
+      }
+    */
 
   /**
    * GetSigT - computes size of the vectorised signature
@@ -327,85 +303,89 @@ namespace {
 		return true;
 	}
 
-  /*
-	template <size_t WIDTH, size_t DEPTH>
-	bool GetLogSigT(const double* src, double* snk, size_t recs)
-	{
-		typedef const double array[WIDTH];
-		const array* source = reinterpret_cast<const array*>(src);
-		typedef double S;
-		typedef double Q;
-		typedef alg::lie<S, Q, WIDTH, DEPTH> LIE;
-		typedef alg::cbh<S, Q, WIDTH, DEPTH> CBH;
-		LIE logans = GetLogSignature<LIE, array, CBH, WIDTH>(&source[0],
-			&source[0 + recs]);
-		unpack_lie_to_SNK<S, LIE, WIDTH, DEPTH>(logans, snk);
-		return true;
-	}
-*/
+    /*
+      template <size_t WIDTH, size_t DEPTH>
+      bool GetLogSigT(const double* src, double* snk, size_t recs)
+      {
+          typedef const double array[WIDTH];
+          const array* source = reinterpret_cast<const array*>(src);
+          typedef double S;
+          typedef double Q;
+          typedef alg::lie<S, Q, WIDTH, DEPTH> LIE;
+          typedef alg::cbh<S, Q, WIDTH, DEPTH> CBH;
+          LIE logans = GetLogSignature<LIE, array, CBH, WIDTH>(&source[0],
+              &source[0 + recs]);
+          unpack_lie_to_SNK<S, LIE, WIDTH, DEPTH>(logans, snk);
+          return true;
+      }
+  */
 }
 
 // A C++ function returning a string of labels
-extern TOSIG_API std::string ShowLogSigLabels(size_t width, size_t depth)
-{
-	//execute the correct Templated Function and return the value
-	try {
-#define TemplatedFn(depth,width) liebasis2stringT<depth,width>()
+extern TOSIG_API std::string ShowLogSigLabels(size_t width, size_t depth) {
+    //execute the correct Templated Function and return the value
+    try {
+#define TemplatedFn(depth, width) liebasis2stringT<depth,width>()
+
 #include "switch.h"
+
 #undef TemplatedFn
-    } catch (std::exception& exc) {
+    } catch (std::exception &exc) {
         PyErr_SetString(PyExc_RuntimeError, exc.what());
     }
-	// only get here if the template arguments are out of range
-	return std::string();
+    // only get here if the template arguments are out of range
+    return std::string();
 }
 
 // A C++ function returning a string of labels
-extern TOSIG_API std::string ShowSigLabels(size_t width, size_t depth)
-{
-	//execute the correct Templated Function and return the value
-	try {
-#define TemplatedFn(depth,width) tensorbasis2stringT<depth,width>()
+extern TOSIG_API std::string ShowSigLabels(size_t width, size_t depth) {
+    //execute the correct Templated Function and return the value
+    try {
+#define TemplatedFn(depth, width) tensorbasis2stringT<depth,width>()
+
 #include "switch.h"
+
 #undef TemplatedFn
-    } catch (std::exception& exc) {
+    } catch (std::exception &exc) {
         PyErr_SetString(PyExc_RuntimeError, exc.what());
     }
-	// only get here if the template arguments are out of range
-	return std::string();
+    // only get here if the template arguments are out of range
+    return std::string();
 }
 
 
 // compute log signature of path at src and place answer in snk
 TOSIG_API int GetLogSig(PyArrayObject *stream, PyArrayObject *snk,
-    size_t width, size_t depth)
- {
+                        size_t width, size_t depth) {
     try {
-    //execute the correct Templated Function and return the value
-#define TemplatedFn(depth,width) GetLogSigT<depth,width>(stream, snk)
+        //execute the correct Templated Function and return the value
+#define TemplatedFn(depth, width) GetLogSigT<depth,width>(stream, snk)
+
 #include "switch.h"
+
 #undef TemplatedFn
-    } catch (std::exception& exc) {
+    } catch (std::exception &exc) {
         PyErr_SetString(PyExc_RuntimeError, exc.what());
     }
     // only get here if the template arguments are out of range
     return false;
- }
+}
 
 // get required size for snk
-TOSIG_API size_t GetLogSigSize(size_t width, size_t depth)
- {
+TOSIG_API size_t GetLogSigSize(size_t width, size_t depth) {
     //execute the correct Templated Function and return the value
     try {
-#define TemplatedFn(depth,width) GetLogSigT<depth,width>()
+#define TemplatedFn(depth, width) GetLogSigT<depth,width>()
+
 #include "switch.h"
+
 #undef TemplatedFn
-    } catch (std::exception& exc) {
+    } catch (std::exception &exc) {
         PyErr_SetString(PyExc_RuntimeError, exc.what());
     }
     // only get here if the template arguments are out of range
     return 0;
- }
+}
 
 // a wrapper un-templated function that calls the correct template instance
 TOSIG_API int GetSig(PyArrayObject *stream, PyArrayObject *snk,
@@ -415,26 +395,29 @@ TOSIG_API int GetSig(PyArrayObject *stream, PyArrayObject *snk,
     try {
 #define TemplatedFn(depth,width) GetSigT<depth,width>(stream, snk)
 #include "switch.h"
+
 #undef TemplatedFn
-    } catch (std::exception& exc) {
+    } catch (std::exception &exc) {
         PyErr_SetString(PyExc_RuntimeError, exc.what());
     }
     // only get here if the template arguments are out of range
     return false;
- }
+}
 
 // get required size for snk
 TOSIG_API const size_t GetSigSize(size_t width, size_t depth)
  {
     //execute the correct Templated Function and return the value
     try {
-#define TemplatedFn(depth,width) GetSigT<depth,width>()
+#define TemplatedFn(depth, width) GetSigT<depth,width>()
+
 #include "switch.h"
+
 #undef TemplatedFn
-    } catch (std::exception& exc) {
+    } catch (std::exception &exc) {
         PyErr_SetString(PyExc_RuntimeError, exc.what());
     }
     // only get here if the template arguments are out of range
     return 0;
- }
+}
 
