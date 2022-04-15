3c6f7c4336e98aac55f8f54354dc58be39355017
inakleinbottle <admin@inakleinbottle.com>
2022-04-15 17:28:14 +0100

    Fixed instlal command for 32 bit

--- build/install-mkl-linux.sh
+++ build/install-mkl-linux.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
+
+
 cp -v build/linux-oneapi-yum-config /etc/yum.repos.d/oneAPI.repo
 
 
 arch=$(uname -m)
+echo $arch
 
 # We can expand this later to get the right libraries on other architectures if necessary
 if [[ $arch =~ ([xX]86_64|[aA][mM][dD]64) ]]; then
     yum install -y intel-oneapi-mkl-devel
 elif [[ $arch =~ ([xX]86|i386|i686) ]]; then
-    yum install -y intel-oneapi-mkl-devel
+    yum install -y intel-oneapi-mkl-devel-32bit
 fi
