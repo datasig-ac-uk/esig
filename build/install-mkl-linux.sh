27638731290e27864be8bb8bafd79667c1df131b
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2022-04-15 17:03:24 +0100

    Update install-mkl-linux.sh

--- build/install-mkl-linux.sh
+++ build/install-mkl-linux.sh
@@ -7,7 +7,7 @@ arch=$(uname -m)
 
 # We can expand this later to get the right libraries on other architectures if necessary
 if [[ $arch =~ ([xX]86_64|[aA][mM][dD]64) ]]; then
-    yum install intel-oneapi-mkl-devel
+    yum install -y intel-oneapi-mkl-devel
 elif [[ $arch =~ ([xX]86|i386|i686) ]]; then
-    yum install intel-oneapi-mkl-devel
+    yum install -y intel-oneapi-mkl-devel
 fi
