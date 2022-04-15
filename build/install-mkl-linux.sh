6f1a4e964eeeb68c9cd1c3c4309f9cae47313016
inakleinbottle <admin@inakleinbottle.com>
2022-04-16 00:19:16 +0100

    openblas-devel

--- build/install-mkl-linux.sh
+++ build/install-mkl-linux.sh
@@ -7,9 +7,9 @@
 arch=$(uname -m)
 echo $arch
 
-cp -v build/linux-oneapi-yum-config /etc/yum.repos.d/oneAPI.repo
 # We can expand this later to get the right libraries on other architectures if necessary
 if [[ $arch =~ ([xX]86_64|[aA][mM][dD]64) ]]; then
+    cp -v build/linux-oneapi-yum-config /etc/yum.repos.d/oneAPI.repo
     yum install -y intel-oneapi-mkl-devel
 elif [[ $arch =~ ([xX]86|i386|i686) ]]; then
     # There is no intel-oneapi-mkl-devel package for 32bit operating systems.
@@ -17,5 +17,5 @@ elif [[ $arch =~ ([xX]86|i386|i686) ]]; then
 #    rpm --import https://yum.repose.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 
 #    yum install -y --skip-broken intel-oneapi-common-vars intel-oneapi-openmp-common intel-oneapi-openmp-32bit intel-oneapi-tbb-32bit intel-oneapi-tbb-common intel-oneapi-mkl-common intel-oneapi-mkl-32bit
-    yum install -y libopenblas-dev
+    yum install -y openblas-devel
 fi
