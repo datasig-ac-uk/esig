0b3ad3d331156a3664312443a518460c6fef1fe5
inakleinbottle <admin@inakleinbottle.com>
2022-04-15 18:36:52 +0100

    Try intel-mkl in the main repo

--- build/install-mkl-linux.sh
+++ build/install-mkl-linux.sh
@@ -7,13 +7,13 @@
 arch=$(uname -m)
 echo $arch
 
+cp -v build/linux-oneapi-yum-config /etc/yum.repos.d/oneAPI.repo
 # We can expand this later to get the right libraries on other architectures if necessary
 if [[ $arch =~ ([xX]86_64|[aA][mM][dD]64) ]]; then
-    cp -v build/linux-oneapi-yum-config /etc/yum.repos.d/oneAPI.repo
     yum install -y intel-oneapi-mkl-devel
 elif [[ $arch =~ ([xX]86|i386|i686) ]]; then
     # There is no intel-oneapi-mkl-devel package for 32bit operating systems.
-    yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
-    rpm --import https://yum.repose.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-    yum install -y intel-mkl-2018.0-033
+#    yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
+#    rpm --import https://yum.repose.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+    yum install -y intel-mkl
 fi
