ed040d5388a1dee5fbda6d5813f9e4ca993a57bf
inakleinbottle <admin@inakleinbottle.com>
2022-08-04 14:00:11 +0100

    Actually implemented caching layer

--- src/paths/src/dyadic_caching_layer.cpp
+++ src/paths/src/dyadic_caching_layer.cpp
@@ -27,7 +27,52 @@ esig::paths::dyadic_caching_layer::log_signature(
         typename esig::paths::path_interface::compute_depth_t resolution,
         const algebra::context &ctx) const
 {
-    std::lock_guard<std::recursive_mutex> access(m_compute_lock);
+    if (empty(domain)) {
+        return ctx.zero_lie(metadata().result_vec_type);
+    }
 
-    return ctx.zero_lie(algebra::vector_type::sparse);
+    if (domain.power() == resolution) {
+        std::lock_guard<std::recursive_mutex> access(m_compute_lock);
+
+        auto& cached = m_cache[domain];
+        if (!cached) {
+            cached = log_signature(domain, ctx);
+        }
+        return cached;
+    }
+
+    dyadic_interval lhs_itvl(domain), rhs_itvl(domain);
+    lhs_itvl.shrink_interval_left();
+    rhs_itvl.shrink_interval_right();
+
+    auto lhs = log_signature(lhs_itvl, resolution, ctx);
+    auto rhs = log_signature(rhs_itvl, resolution, ctx);
+
+    return ctx.cbh({lhs, rhs}, metadata().result_vec_type);
+}
+
+esig::algebra::lie
+esig::paths::dyadic_caching_layer::log_signature(
+    const esig::interval &domain,
+    esig::paths::path_interface::compute_depth_t resolution,
+    const esig::algebra::context &ctx) const
+{
+    // For now, if the ctx depth is not the same as md depth just do the calculation without caching
+    // be smarter about this in the future.
+    const auto& md = metadata();
+    assert(ctx.width() == md.width);
+    if (ctx.depth() != md.depth) {
+        return path_interface::log_signature(domain, resolution, ctx);
+    }
+
+    auto dyadic_dissection = esig::to_dyadic_intervals(domain.inf(), domain.sup(), resolution);
+
+    std::vector<algebra::lie> lies;
+    lies.reserve(dyadic_dissection.size());
+    for (const auto& itvl : dyadic_dissection) {
+        auto lsig = log_signature(itvl, resolution, ctx);
+        lies.push_back(lsig);
+    }
+
+    return ctx.cbh(lies, metadata().result_vec_type);
 }
