f95bdbc4030cb031db335d8bf9837e267a6d35c5
Sam Morley <sam@inakleinbottle.com>
2022-12-12 10:36:01 +0000

    Initialise keys and key iterators

--- src/pyesig/py_algebra.cpp
+++ src/pyesig/py_algebra.cpp
@@ -6,9 +6,18 @@
 
 #include "free_tensor.h"
 #include "lie.h"
+#include "py_lie_key.h"
+#include "py_lie_key_iterator.h"
+#include "py_tensor_key.h"
+#include "py_tensor_key_iterator.h"
 
 
 void esig::python::init_algebra(py::module_ &m) {
+
+    init_py_lie_key(m);
+    init_py_tensor_key(m);
+    init_lie_key_iterator(m);
+    init_tensor_key_iterator(m);
 
     init_lie(m);
     init_free_tensor(m);
